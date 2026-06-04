048b2a8616d67648735821d57f67e8ea1bf8632c
Nayan Das <nayan010das@gmail.com>
2026-06-04 21:58:07 +0530

    Deployment Issue Fixed

--- app/(auth)/verify-email/page.jsx
+++ app/(auth)/verify-email/page.jsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { FcSportsMode } from "react-icons/fc";
 
@@ -37,7 +37,7 @@ const IconCheck = () => (
 
 export default function VerifyEmailPage() {
   const router = useRouter();
-  const searchParams = useSearchParams();
+
   const [email, setEmail] = useState("");
 
   useEffect(() => {
